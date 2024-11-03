b7a19d57f089b9379589ccb5ff3e4753ff472cc1
diegooo01 <129528729+diegooo01@users.noreply.github.com>
2024-11-02 22:06:19 -0500

    Initial commit

---
+++ src/hidrants-net/hidrants-net-content.component.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: 'hidrants-net-content'
+}
+</script>
+
+<template>
+  <div class="hidrants-net-content">
+    <h1>Red de Hidrantes del Callao</h1>
+    <p>Visualiza la Red óptima de hidrantes para su correcto mantenimiento. Con esta red <br> se tendrá un mantenimiento mucho más eficiente y exitoso de los hidrantes de la ciudad del Callao.</p>
+    <div class="map-visualizer">
+      <h1>MAP</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.hidrants-net-content{
+  padding: 5rem 2rem;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.hidrants-net-content h1,p{
+  color: #4E171C;
+  text-align: center;
+}
+
+.hidrants-net-content p{
+  padding-top: 1.5rem;
+}
+
+
+.map-visualizer{
+  padding-top: 2rem;
+}
+
+</style>
---
+++ src/nearest-hidrant/nearest-hidrant-content.component.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: 'nearest-hidrant-content'
+}
+</script>
+
+<template>
+  <div class="nearest-content">
+    <h1>Los hidrantes más cercanos a ti...</h1>
+    <p>Dale click a una zona del mapa del Callao para determinar <br> tu ubicación y darte la ruta más corta para el hidrante de la capacidad deseada y más cercano a ti.</p>
+    <div class="map-visualizer">
+      <h1>MAP</h1>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+.nearest-content{
+  padding: 5rem 2rem;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.nearest-content h1,p{
+  color: #4E171C;
+  text-align: center;
+}
+
+.nearest-content p{
+  padding-top: 1.5rem;
+}
+
+.map-visualizer{
+  padding-top: 2rem;
+}
+
+</style>
